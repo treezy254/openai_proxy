0ab9ee4cd43b698cd69cd9e07ba9f00ef01b56cb
Samuel Kamau <gachungasamuel54@gmail.com>
2024-07-10 07:16:05 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+!wget -q https://raw.githubusercontent.com/treezy254/ollama-companion/master/install.sh && sudo chmod +x install.sh 2>&1 /dev/null
+# Sets up Latest version of Llama.cpp for quanting.
+# If You need to build a new one use the argument `-colab_compile`
+# Also interactive installer available use -interactive or -i
+# Using a virtual environment is not recommended within Google_Colab
+# Docs are embedded within Ollama-Companion
+!/content/install.sh -colab
